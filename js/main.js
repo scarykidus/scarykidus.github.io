78559c941305fd799c807952ddc3b8ddd4d5ede4
Oleg Obukhov <37498934+scarykidus@users.noreply.github.com>
2020-04-24 17:14:16 +0300

    Files for site

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+       var select = document.querySelector('.select'),
+           listBooks = document.getElementById('listBooks'),
+           textField = document.querySelector('.text_field'),
+           selectItem = document.querySelectorAll('.select__item');
+
+       addEventListener('click', function (event) {
+        document.querySelector('.select').style.display = "block";
+           if (event.target == listBooks) {
+               if (listBooks.classList.contains('pushed')) {
+                   HideList();
+               } else {
+                   ShowList();
+               }
+           } else {
+              selectItem.forEach(function (el) {
+                   if (el.contains(event.target)) {
+                       textField.innerHTML = event.target.innerHTML;
+                       textField.style.fontSize = '13px';
+                   }
+               });
+           }
+       });
+
+       function ShowList() {
+           listBooks.classList.add('pushed');
+           select.classList.remove('fadeOut');
+           select.classList.add('fadeIn');
+           select.classList.add('showlist');
+       }
+});
+
+
